1fc12430af83304a82523d3ee7350aad63818571
Aviral Dwivedi <aviraldwivedi410@gmail.com>
2026-08-18 10:18:39 +0530

    lab3

--- Lab3/app.js
+++ Lab3/app.js
@@ -1,10 +1,14 @@
 import http from 'http'
 
-const server=http.createServer()
+/*const server=http.createServer()
 server.on('request',(req,res)=>{
     res.write("<h1>Welcome to server side programming</h1>")
     res.write("Nodemon is tracking files")
     res.end()
+})*/
+
+const server=http.createServer((req,res)=>{
+    res.end("<h2>Welcome to server side</h2>")
 })
 
 server.listen(5000,()=>{
